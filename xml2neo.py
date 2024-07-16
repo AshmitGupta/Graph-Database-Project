f24735fd416a39015582149ea5332db636a5b648
AshmitGupta <67188586+AshmitGupta@users.noreply.github.com>
2024-07-16 09:05:08 -0700

    Bidirectionality implemented

--- xml2neo.py
+++ xml2neo.py
@@ -18,8 +18,10 @@ def create_nodes_and_relationships(parent_node, element):
         if child.tag == 'effectivity':
             effectivity_node = Node(child.tag, name=child.tag)
             relationship = Relationship(parent_node, child.tag, effectivity_node)
+            reverse_relationship = Relationship(effectivity_node, f"has_{parent_node['name']}", parent_node)
             graph.create(effectivity_node)
             graph.create(relationship)
+            graph.create(reverse_relationship)
 
             airplanes_element = child.find('airplanes')
             if airplanes_element is not None:
@@ -40,7 +42,9 @@ def create_nodes_and_relationships(parent_node, element):
                     airplane_node = Node("Airplane", name=airplane_type)
                     graph.create(airplane_node)
                     effectivity_relationship = Relationship(effectivity_node, "effects", airplane_node)
+                    reverse_effectivity_relationship = Relationship(airplane_node, f"affected_by", effectivity_node)
                     graph.create(effectivity_relationship)
+                    graph.create(reverse_effectivity_relationship)
                     print(f"Created Airplane node: {airplane_type}")
                     
                     # Create or connect to existing LineNumber nodes and relationships
@@ -50,15 +54,19 @@ def create_nodes_and_relationships(parent_node, element):
                             line_number_node = Node("LineNumber", name=line_number)
                             graph.create(line_number_node)
                         line_relationship = Relationship(airplane_node, "includes", line_number_node)
+                        reverse_line_relationship = Relationship(line_number_node, f"included_in", airplane_node)
                         graph.create(line_relationship)
+                        graph.create(reverse_line_relationship)
                         print(f"Created or connected to LineNumber node: {line_number}")
         else:
             # Handle other nodes normally
             content = (child.text or "").strip()
             node = Node(child.tag, name=child.tag, content=content)
             relationship = Relationship(parent_node, child.tag, node)
+            reverse_relationship = Relationship(node, f"has_{parent_node['name']}", parent_node)
             graph.create(node)
             graph.create(relationship)
+            graph.create(reverse_relationship)
             create_nodes_and_relationships(node, child)
 
 # Process each XML file
